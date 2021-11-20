222171773c8538d535d3afbb350c167ede17cb75
Naincy224 <pihusingh224@gmail.com>
2021-11-20 07:52:28 +0530

    create a Stack of

---
+++ src/com/bridgeit/MainStack.java
@@ -0,0 +1,23 @@
+package com.bridgeit;
+
+public class MainStack {
+	 public static void main(String[] args)
+	    {
+	        
+	        StackUsingLinkedlist obj = new StackUsingLinkedlist();
+	        
+	        obj.push(70);
+	        obj.push(30);
+	        obj.push(56);
+	        
+	 
+	        
+	        obj.display();
+	 
+	        
+	        
+	 
+	    
+	    }
+	
+}
---
+++ src/com/bridgeit/Node.java
@@ -0,0 +1,7 @@
+package com.bridgeit;
+
+public class Node {
+
+	int data; // integer data
+	Node link; // reference variable Node type
+}
